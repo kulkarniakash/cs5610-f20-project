4b45c2ac796f3e5168cf566620eb35b18f2390ff
kulkarniakash <kulkarni.akash@gmail.com>
2020-10-20 15:06:04 -0400

    Added very basic user authentication from auth0

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,32 @@
 import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {useAuth0} from "@auth0/auth0-react";
 
 const NavBar = () => {
+    const {loginWithRedirect} = useAuth0();
+    const {logout} = useAuth0();
+    const {isAuthenticated} = useAuth0();
     return (
         <nav className="navbar navbar-inverse" style={{borderRadius: 0}}>
             <ul className="nav navbar-nav">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About Us</a></li>
+
+                {isAuthenticated &&
+                <Route path='/signed-in'>
+                    <li>
+                        <a href='#'>My Songs</a>
+                    </li>
+                </Route>}
             </ul>
             <ul className='nav navbar-nav navbar-right' style={{marginRight: '20px'}}>
-                <li><a href='#'>Sign in</a></li>
+                <li><a onClick={() => loginWithRedirect()}>Sign in</a></li>
+                {isAuthenticated &&
+                <Route path='/signed-in'>
+                    <li><a onClick={() => logout(
+                        {returnTo: window.location.origin}
+                        )}>Log Out</a></li>
+                </Route>}
             </ul>
         </nav>
     )
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Auth0Provider} from "@auth0/auth0-react";
+import {BrowserRouter as Router} from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <Auth0ProviderWithHistory>
+              <App />
+          </Auth0ProviderWithHistory>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
