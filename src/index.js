16acf7e4d2156cd58753bb962d8a9ea9bc46907b
kulkarniakash <kulkarni.akash@gmail.com>
2020-11-02 23:14:59 -0500

    removed auth0

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Auth0Provider} from "@auth0/auth0-react";
 import {BrowserRouter as Router} from "react-router-dom";
-import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+
 
 ReactDOM.render(
   <React.StrictMode>
       <Router>
-          <Auth0ProviderWithHistory>
-              <App />
-          </Auth0ProviderWithHistory>
+          <App />
       </Router>
   </React.StrictMode>,
   document.getElementById('root')
