e2066519a5cd69ace6c96221d7f40056d5c3af6b
kulkarniakash <kulkarni.akash@gmail.com>
2020-12-14 23:45:29 -0500

    redirect url modified for production

--- src/constants/spotifyAPIConstants.js
+++ src/constants/spotifyAPIConstants.js
@@ -3,8 +3,8 @@
 export const CLIENT_ID = "08b8c56399c848388c05769966c722e2";
 export const SPOTIFY_ACCOUNT_URL = "https://accounts.spotify.com";
 // change to heroku link for production!
-// export const AUTH_REDIRECT_URI = "https://immense-temple-17196.herokuapp.com/";
-export const AUTH_REDIRECT_URI = "http://localhost:3000/";
+export const AUTH_REDIRECT_URI = "https://immense-temple-17196.herokuapp.com/";
+// export const AUTH_REDIRECT_URI = "http://localhost:3000/";
 
 // export const JAVA_SERVER_URL = "http://localhost:8080/";
 export const JAVA_SERVER_URL = "https://serene-brushlands-40448.herokuapp.com/"
